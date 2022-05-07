f891131fadb23b9d3f3f15ff325acffbf4b41e31
Victor Kane <victorkane@gmail.com>
2022-05-07 12:08:31 -0300

    Explore Project Structure: Live Reload conditional on dev or prod mode

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,11 @@ export default function App() {
         <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        Hello world
-        <LiveReload />
+        Hello, world!
+        {process.env.NODE_ENV === "development"
+          ? console.log("yes: dev mode")
+          : null}
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
   )
