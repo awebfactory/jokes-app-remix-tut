9649f2c06ccaf6652c3ed59eb94f064597037338
Victor Kane <victorkane@gmail.com>
2022-05-07 12:24:42 -0300

    Routes: index route

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { LiveReload } from "@remix-run/react"
+import { LiveReload, Outlet } from "@remix-run/react"
 
 export default function App() {
   return (
@@ -8,10 +8,7 @@ export default function App() {
         <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        Hello, world!
-        {process.env.NODE_ENV === "development"
-          ? console.log("yes: dev mode")
-          : null}
+        <Outlet />
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
